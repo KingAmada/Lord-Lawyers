109d772d43086e5739072c7fb762bc69fad165a4
KingAmada <sarkiamada@gmail.com>
2024-12-16 22:02:04 -0800

    Update generate-audio.js

--- api/generate-audio.js
+++ api/generate-audio.js
@@ -1,16 +1,6 @@
+// api/generate-audio.js
+
 const fetch = require('node-fetch');
-
-// Four parts of the API key (replace these with your actual parts)
-
-const PART1 = 'sk-proj-Q5u0eI1D_8hntsp3';
-const PART2 = '_utd4HuSQfaDF-IvPRZ';
-const PART3 = 'ZV4e0E57MPoetQ8IZ0';
-const PART4 = '5VJIUpq';
-const PART5 = '9MGx3Lm8-m72XzT3BlbkFJcYiW1RP9zhYLbf';
-const PART6 = '-aN7RJEVaqO88kQcFQ5aBZe';
-const PART7 = '_TbUStSYcThMNh-d8uEu-k8CmVa-Q9A2zYNQA';
-
-const openai_api_key = PART1 + PART2 + PART3 + PART4 + PART5 + PART6 + PART7;
 
 module.exports = async (req, res) => {
     if (req.method !== 'POST') {
@@ -26,6 +16,8 @@ module.exports = async (req, res) => {
     }
 
     try {
+        const openai_api_key = process.env.OPENAI_API_KEY;
+
         // List of supported voices
         const supportedVoices = ['nova', 'shimmer', 'echo', 'onyx', 'fable', 'alloy'];
 
